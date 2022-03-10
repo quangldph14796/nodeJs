afaa0ae858caa784029f14817e5e6affbb5d7f1d
Quang Duy <quangld@gmail.com>
2022-03-10 10:26:24 +0700

    ????

---
+++ routers/product.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+const router = Router();
+
+const check = (req, res, next) => {
+  const status = true;
+  if (status) {
+    next();
+  } else {
+    console.log("Anh có cái nịt");
+  }
+};
+router.get("/api/products", check, (req, res) => {
+  const products = [
+    { id: 1, name: "Hảo Hán 1" },
+    { id: 2, name: "Hảo Hán 2" },
+  ];
+  res.json(products);
+});
+router.post("/api/products", check, (req, res) => {
+  console.log(req.body);
+  const products = [
+    { id: 1, name: "Product A" },
+    { id: 2, name: "Product B" },
+  ];
+  products.push(req.body);
+  res.json(products);
+});
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,42 +1,13 @@
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-//     console. log('url', req.url);
-//     if(req.url === "/"){
-//         res.setHeader ('Content-Type',"text/html");
-//          res.write("shtml><body><h1>Home Page</h1></body>
-//          res.end();
-//      } else if(req.url === "/api/products"){
-//          const products = [
-//              {id: 1, name: "Product A"},
-//              {id: 2, name: "Product B"}
-//         res.end (JSON. stringify(products));
-//      } else {
-//          console. log('Giờ thì em biết rồi');
-//      }
-//     });
-
-// const check = (reg, reg, next) => {
-//   const status = false;
-//   if (status) {
-//     console.log("chào");
-//   } else {
-//     console.log("có, có cái nịt");
-//   }
-// };
-// app.get("/api/products", check, (req, res) => {
-//   const products = [
-//     { id: 1, name: "Product A" },
-//     { id: 2, name: "Product B" },
-//   ];
-//   res.json(products);
-// });
-
-/**
- * npm i -g json-server: Cài đặt module vào ổ hệ thống
- * npm i --save express:
-     - Khi sử dụng npm i mặc định được cài đặt luôn
-     - Bắt buộc phải có module này thì mới chạy project
-  * dependencies: { "express": 1.2.3 }
- * npm i --save-dev nodemon
- * 
- */
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import productRoute from "../routers/product.js";
+const app = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(productRoute);
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
