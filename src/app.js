60ddde2c1db21e0f56985121f9187307fbbcf86a
Quang Duy <quangld@gmail.com>
2022-03-10 11:11:09 +0700

    buoi 2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import productRoute from "../routers/product.js";
+import productRoute from "../routes/product";
+
 const app = express();
+// middleware
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(productRoute);
+
+app.use("/api", productRoute);
+
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log("Server is running port", PORT);
 });
