27341740b74d46c76538fcb98172afd3afe3c0b3
Quang Duy <quangld@gmail.com>
2022-03-12 10:13:10 +0700

    change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import productRoute from "../routes/product";
+import productRoute from "../routers/product";
 
 const app = express();
 // middleware
